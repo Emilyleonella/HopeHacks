76048026fccba947d05733fd6407484914b0f114
Emily Tamayo <EmilyLeonella@R2Hs-MBP.lan>
2022-02-17 19:48:58 -0500

    got server to show css

--- server.js
+++ server.js
@@ -6,34 +6,12 @@ const express = require('express')
 const app = express()
 
 
-app.use(express.static(__dirname + "./"))
+app.use(express.static("public"))
 
 app.get('/', (req, res) =>{
 res.sendFile(path.join(__dirname, "./index.html"))
 })
 
-console.log(__dirname + "/style.css");
 app.listen(port)
 
 
-
-// const server = http.createServer(function(req,res){
-// res.writeHead(200,{'Content-Type': 'text/html'})
-// fs.readFile('index.html', function(err, data){
-//   if(err){
-//     res.writeHead(404)
-//     res.write('This is not the page you are looking for')
-//   }else{
-//     res.write(data)
-//   }
-//   res.end()
-// })
-// })
-
-// server.listen(port, function(err){
-//   if(err){
-//     console.log('Something went wrong', error)
-//   }else{
-//     console.log('Server is listening to port' + port)
-//   }
-// })
