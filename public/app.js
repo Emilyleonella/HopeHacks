93d637b4d98fd230e743898ac17c29558733b80a
ashleylozz02 <92343168+ashleylozz02@users.noreply.github.com>
2022-02-21 21:27:26 -0500

    update Ashley-Branch

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-
+function submitForm() {
+  let form = document.getElementById("form__submit");
+  form.submit();
+}
+("use strict");
+fetch("./data_class.json")
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {});
--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-const http = require('http')
-const fs = require('fs')
-const port = 5000
-const path = require('path');
-const express = require('express');
-const res = require('express/lib/response');
-const app = express()
+const http = require("http");
+const fs = require("fs");
+const port = 5000;
+const path = require("path");
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
 
+const math = require("mathjs");
 
-const simplify = require('semver/ranges/simplify');
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
 
+app.get("/", (req, res) => {});
+///app.post('/',(req,res)=>{
+//let url=`http://api.mathjs.org/v4/`
+//})
 
-
-
-
-
-
-app.use(express.static("public"))
-
-app.get('/', (req, res) =>{
-res.sendFile(path.join(__dirname, "./index.html"))
-})
-
-app.get('/', (req, res) => {
-    
-})
-
-app.listen(port)
-
-
+app.post;
+app.listen(port);
