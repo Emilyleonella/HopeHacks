4de2e81c97044201ce23df7fdcda93b792c638cd
Emily Tamayo <EmilyLeonella@R2Hs-MBP.lan>
2022-02-22 21:39:18 -0500

    updating

--- public/app.js
+++ public/app.js
@@ -1,37 +1,14 @@
-// const total = function getAnswers() {
-//    let values = [];
-//   let q1 = document.getElementsById('input[name = q1]:checked').value;
-//   let q2 = document.getElementById('input[name = q2]:checked').value;
-//   let q3 = document.getElementById('input[name = q3]:checked').value;
-//   let q4 = document.getElementById('input[name = q4]:checked').value;
-//   let q5 = document.getElementById('input[name = q1]:checked').value;
 
-
-//   values.push(document.getElementsById("questionOne").value)
-//   values.push(document.getElementById("questionTwo").value);
-//   values.push(document.getElementById("questionThree").value);
-//   values.push(document.getElementById("questionFour").value);
-//   values.push(document.getElementById("questionFive").value);
-  
-// return values;
-
-
-// };
-
-// console.log(total)
-
-
-
-let radioBtns = document.querySelectorAll('input[name = "q1"]');
+let radioBtns = document.querySelectorAll('input[name = "q1"]'); // to select radio buttions by name ////////
 let radioBtns2 = document.querySelectorAll('input[name = "q2"]');
 let radioBtns3 = document.querySelectorAll('input[name = "q3"]');
 let radioBtns4 = document.querySelectorAll('input[name = "q4"]');
 let radioBtns5 = document.querySelectorAll('input[name = "q5"]');
-let qValues = [];
+let qValues = []; // empty array to push values ///////////
 
 let findSelected = () => {
-  let selected = document.querySelector("input[name = 'q1']:checked").value
-  qValues.push(selected)
+  let selected = document.querySelector("input[name = 'q1']:checked").value // function to store the value from the checked radio buttons 
+  qValues.push(selected) // value is pushed into empty array ///////////
  console.log(qValues)
 }
 let findSelected2 = () => {
@@ -56,20 +33,11 @@ let findSelected5 = () => {
   let selected = document.querySelector("input[name = 'q5']:checked").value
   qValues.push(selected)
   console.log(qValues)
-  // let number = parseFloat(qValues[0])
-  // let number2 = parseFloat(qValues[1])
-  // let number3 = parseFloat(qValues[2])
-  // let number4 = parseFloat(qValues[3])
-  // let number5 = parseFloat(qValues[4])
-  // console.log(number)
-  // console.log(number)
-  // console.log(number2)
-  // console.log(number3)
-
+ 
 }
   
 
-radioBtns.forEach(radioBtn =>{
+radioBtns.forEach(radioBtn =>{ 
   radioBtn.addEventListener("change",
   findSelected);
 })
@@ -92,7 +60,7 @@ radioBtns5.forEach(radioBtn =>{
 })
 
 
-console.log(qValues)
+// console.log(qValues)
 
 //Submit button 
 // fire submit onclick 
@@ -100,27 +68,37 @@ console.log(qValues)
     // post sum to website for user to see
     // clear the array and all form values
 
+
+/////// function for onsubmit 
     function submitForm (){
-    console.log(qValues)
-      var sum = 0;
-      for(let i = 0; i < qValues.length; i++){
-        sum += qValues[i];
+      let number = parseFloat(qValues[0])
+      let number2 = parseFloat(qValues[1]) //storing the string values into numbers and sotring them into variables 
+      let number3 = parseFloat(qValues[2])
+      let number4 = parseFloat(qValues[3])
+      let number5 = parseFloat(qValues[4])
+
+      //let totalGallons = math.evaluate`${number}+${number2}+${number3}+${number4}+${number5}+` /////// math.evaluate is from mathjs api its working on server.js but not inside the Public folder 
+      //console.log(totalGallons)
+
+      // here I will add the values from the variables to get the sum ///////////
       }
-       //return sum;
-      console.log(sum)
-    }
+
+// another function I was working on ////////////////////
+    // function submitForm (){
+    // console.log(qValues)
+    //   var sum = 0;
+    //   for(let i = 0; i < qValues.length; i++){
+    //     sum += qValues[i];
+    //   }
+    //    //return sum;
+    //   console.log(sum)
+    // }
 
 
 let myForm = document.getElementById('fromQues')
 
 myForm.addEventListener('submit', (event) => {
-  event.preventDefault() // prevent form from auto submiting 
+  event.preventDefault() // prevent form from auto submiting /////////////
 
-  // const request = new XMLHttpRequest();
-
-  // request.open("post", "form.php")
-  // request.onload = function(){
-  //   console.log(request.responseText)
-  // }
-  // request.send(new FormData(myForm))
 })
+
